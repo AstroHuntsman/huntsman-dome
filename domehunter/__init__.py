0f508f4f6e54528eed7e06cfed1b8b343863fdf9
Lee Spitler <lee.spitler@mq.edu.au>
2019-05-16 20:07:00 +1000

    setup class structure (#9)
    * astropy stuff
    
    * class skeletons
    
    * example move
    
    * add startup script
    
    * reorg, simplify class and RPC prep
    
    * example/move_ccw.py works
    
    * small fixes from wilfred
    
    * python version

--- domehunter/__init__.py
+++ domehunter/__init__.py
@@ -1,8 +1,13 @@
+"""Run dome control on a raspberry pi GPIO."""
+
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 # Packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
+from gpiozero import DigitalInputDevice, Device
+from gpiozero.pins.mock import MockFactory
+
 from ._astropy_init import *
 # ----------------------------------------------------------------------------
 
@@ -10,14 +15,141 @@ from ._astropy_init import *
 # This is the same check as the one at the top of setup.py
 import sys
 
-__minimum_python_version__ = "3.7"
+__minimum_python_version__ = "3.6"
+
 
 class UnsupportedPythonError(Exception):
     pass
 
+
 if sys.version_info < tuple((int(val) for val in __minimum_python_version__.split('.'))):
-    raise UnsupportedPythonError("domehunter does not support Python < {}".format(__minimum_python_version__))
+    raise UnsupportedPythonError(
+        "domehunter does not support Python < {}".format(__minimum_python_version__))
 
 if not _ASTROPY_SETUP_:
     # For egg_info test builds to pass, put package imports here.
-    from .example_mod import *
+    pass
+
+
+class X2DomeRPC():
+    """Dummy class until real RPC class is available."""
+
+    def __init__(self, *args, **kwargs):
+        """Create dummy class until real RPC class is available."""
+
+
+class Dome(X2DomeRPC):
+    """
+    Interface to dome control raspberry pi GPIO.
+
+    Might start with: https://gpiozero.readthedocs.io/en/stable/
+    but might use something else if we buy a fancy Pi HAT.
+    """
+
+    def __init__(self, testing=True, *args, **kwargs):
+        """Initialize raspberry pi GPIO environment."""
+        if testing:
+            # Set the default pin factory to a mock factory
+            Device.pin_factory = MockFactory()
+
+            ENCODER_PIN_NUMBER = 7
+            HOME_SENSOR_PIN_NUMBER = 11
+        else:
+            """ Do not change until you're sure!!! """
+            # https://pinout.xyz/pinout/automation_hat
+            ENCODER_PIN_NUMBER = None
+            HOME_SENSOR_PIN_NUMBER = None
+
+        self.dome_status = "unknown"
+
+        self.encoder_count = 0
+        self.encoder = DigitalInputDevice(ENCODER_PIN_NUMBER)
+        # _increment_count function to run when encoder is triggered
+        self.encoder.when_activated = self._increment_count
+
+        self._at_home = False
+        self.home_sensor = DigitalInputDevice(HOME_SENSOR_PIN_NUMBER)
+        # _set_not_home function is run when home sensor is NOT being triggered
+        self.home_sensor.when_deactivated = self._set_not_home
+        # _set_at_home function is run when home sensor is triggered
+        self.home_sensor.when_activated = self._set_at_home
+
+##################################################################################################
+# Properties
+##################################################################################################
+
+    @property
+    def is_home(self):
+        """Send True if the dome is at home."""
+        return self._at_home
+
+    @property
+    def status(self):
+        """Return a text string describing dome rotators current status."""
+        pass
+
+
+##################################################################################################
+# Methods
+##################################################################################################
+
+    """These map directly onto the AbstractMethods created by RPC."""
+
+    def abort(self):
+        """Stop everything."""
+        pass
+
+    def getAzEl(self):
+        """Return AZ and Elevation."""
+        pass
+
+    def start_daemon(self):
+        """Maybe start the RCP daemon here?."""
+        raise NotImplementedError
+
+    def halt_daemon(self):
+        """Maybe start the RCP daemon here?."""
+        raise NotImplementedError
+
+##################################################################################################
+# Private Methods
+##################################################################################################
+
+    def _set_at_home(self):
+        self._at_home = True
+
+    def _set_not_home(self):
+        self._at_home = False
+
+    def _increment_count(self, device=None):
+        print(f"{device} activated _increment_count")
+        if self.current_direction == "CW":
+            self.encoder_count += 1
+        elif self.current_direction == "CCW":
+            self.encoder_count -= 1
+        elif self.current_direction is None:
+            if self.last_direction == "CW":
+                self.encoder_count += 1
+            elif self.last_direction == "CCW":
+                self.encoder_count -= 1
+
+    def _move_west(self, degrees_west):
+        print(f"Moving {degrees_west} West")
+        if self._west_is_ccw:
+            cmd_status = self.move_ccw(degrees_west)
+        else:
+            cmd_status = self.move_cw(degrees_west)
+
+        return cmd_status
+
+    def _move_cw(self, degrees):
+        print(f"Sending GPIO move_cw({degrees}) command.")
+        self.current_direction = "CW"
+        cmd_status = True
+        return cmd_status
+
+    def _move_ccw(self, degrees):
+        print(f"Sending GPIO move_ccw({degrees}) command.")
+        self.current_direction = "CCW"
+        cmd_status = True
+        return cmd_status
---
+++ examples/move_ccw.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""Example of how to move the dome CCW."""
+from domehunter import Dome
+from astropy import units as u
+
+
+def main(degrees_ccw,
+         **kwargs):
+    """Example of moving the dome CCW.
+
+    See argparse help string below for details about parameters.
+    """
+    dome = Dome()
+    dome_moved = dome._move_ccw(degrees_ccw)
+    return dome_moved
+
+
+if __name__ == '__main__':
+
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Example dome move to the CCW")
+    parser.add_argument('--degrees_ccw', default=10,
+                        help='Number of degrees CCW to move.')
+    parser.add_argument('--verbose', action='store_true', default=False, help='Verbose.')
+
+    args = parser.parse_args()
+    args.degrees_ccw = float(args.degrees_ccw) * u.degree
+
+    success = main(**vars(args))
+    if args.verbose:
+        if success:
+            print(f"Dome successfully moved {args.degrees_ccw}")
+        else:
+            print("Error: Dome did not move")
