1f394a7dafade6d06cfd7fcaf35815a983e75b81
fergus <fergus.longbottom@hdr.mq.edu.au>
2019-07-18 19:16:42 +1000

    added a testing mode

--- domehunter/__init__.py
+++ domehunter/__init__.py
@@ -55,21 +55,14 @@ class Dome():
             # relay 2 on automation hat
             # on position for CW and off for CCW?
             DIRECTION_RELAY_PIN_NUMBER = 19
-            # NB I'm just going to move this entirely into a tests file
-            # to create test device just do the following
-            """
-            encoder = DigitialInputDevice(ENCODER_PIN_NUMBER, bounce_time=0.1)
-            # create reference to the mock pin used by the device
-            encoder_pin = Device.pin_factory.pin(ENCODER_PIN_NUMBER)
-            # then toggle the pin high/low to simulate encoder ticks/home home
-            # sensor or activation of the relays.
-            encoder_pin.drive_low()
-            time.sleep(0.1)
-            encoder_pin.drive_high()
-            """
+            # set a timeout length in seconds for wait_for_active() calls
+            WAIT_TIMEOUT = 1
+            # set a variable for bounce_time in seconds
+            BOUNCE_TIME = 0.1
+
+            self.encoder_pin = Device.pin_factory.pin(ENCODER_PIN_NUMBER)
         else:
             """ Do not change until you're sure!!! """
-            # https://pinout.xyz/pinout/automation_hat
             # input 1 on automation hat
             ENCODER_PIN_NUMBER = 26
             # input 2 on the automation hat
@@ -79,17 +72,24 @@ class Dome():
             # relay 2 on automation hat
             # on position for CW and off for CCW?
             DIRECTION_RELAY_PIN_NUMBER = 19
+            # set the timeout length variable to None for non testing mode
+            WAIT_TIMEOUT = None
+            # set a variable for bounce_time in seconds
+            BOUNCE_TIME = 0.1
 
         # initialize status and az as unknown, to ensure we have properly
         # calibrated az
+        self.testing = testing
         self.dome_status = "unknown"
         self.dome_az = None
+        self.current_direction = None
 
         # create a instance variable to track the dome motor encoder ticks
         self.encoder_count = 0
         # bounce_time settings gives the time in seconds that the device will
         # ignore additional activation signals
-        self.encoder = DigitalInputDevice(ENCODER_PIN_NUMBER, bounce_time=0.1)
+        self.encoder = DigitalInputDevice(
+            ENCODER_PIN_NUMBER, bounce_time=BOUNCE_TIME)
         # _increment_count function to run when encoder is triggered
         self.encoder.when_activated = self._increment_count
         # dummy value
@@ -97,7 +97,7 @@ class Dome():
 
         self._at_home = False
         self.home_sensor = DigitalInputDevice(
-            HOME_SENSOR_PIN_NUMBER, bounce_time=0.1)
+            HOME_SENSOR_PIN_NUMBER, bounce_time=BOUNCE_TIME)
         # _set_not_home function is run when upon home senser deactivation
         self.home_sensor.when_deactivated = self._set_not_home
         # _set_at_home function is run when home sensor is activated
@@ -113,6 +113,11 @@ class Dome():
             ROTATION_RELAY_PIN_NUMBER, initial_value=False)
         self.direction_relay = DigitalOutputDevice(
             DIRECTION_RELAY_PIN_NUMBER, initial_value=False)
+
+        # set a wait time for testing mode that exceeds BOUNCE_TIME
+        self.t_wait = BOUNCE_TIME + 0.05
+        # set the timeout for wait_for_active()
+        self.wait_timeout = WAIT_TIMEOUT
 
 ###############################################################################
 # Properties
@@ -136,7 +141,7 @@ class Dome():
 
     def abort(self):
         """Stop everything."""
-        # this could simply call the self._stop_moving() method?
+        self._stop_moving()
         pass
 
     def getAz(self):
@@ -150,77 +155,89 @@ class Dome():
         if self.dome_az is None:
             self.calibrate()
         delta_az = az - self.dome_az
+
+        # determine whether CW or CCW gives the short path to desired az
         if abs(delta_az) > 180:
             if delta_az > 0:
                 delta_az -= 360
             else:
                 delta_az += 360
 
+        # if updated delta_az is positive, direction is CW
         if delta_az > 0:
+            # converted delta_az to equivilant in encoder ticks
             ticks = self._az_to_ticks(delta_az)
             target_position = self.encoder_count + ticks
             self._move_cw()
-            while self.encoder_count <= target_position:
+            # wait until encoder count matches desired delta az
+            while self.encoder_count < target_position:
+                if self.testing:
+                    # if testing simulate a tick for every cycle of while loop
+                    self._simulate_ticks(num_ticks=1)
                 pass
             self._stop_moving()
             # compare original count to current just in case we got more ticks
             # than we asked for
             old_encoder_count = target_position - ticks
+            # update dome_az based on the actual number of ticks counted
             self.dome_az += self._ticks_to_az(
                 self.encoder_count - old_encoder_count)
+            # take mod360 of dome_az to keep 0 <= dome_az < 360
             self.dome_az %= 360
+            # update encoder_count to match the dome_az
             self.encoder_count = self._az_to_ticks(self.dome_az)
             pass
+
+        # if updated delta_az is negative, direction is CCW
         if delta_az < 0:
-            # here ticks is going to be negative
+            # converted delta_az to equivilant in encoder ticks
             ticks = self._az_to_ticks(delta_az)
             target_position = self.encoder_count + ticks
             self._move_ccw()
+            # wait until encoder count matches desired delta az
             while self.encoder_count >= target_position:
+                if self.testing:
+                    # if testing simulate a tick for every cycle of while loop
+                    self._simulate_ticks(num_ticks=1)
                 pass
             self._stop_moving()
             # compare original count to current, just in case we got more ticks
             # than we asked for
-
             old_encoder_count = target_position - ticks
+            # update dome_az based on the actual number of ticks counted
             self.dome_az += self._ticks_to_az(
                 self.encoder_count - old_encoder_count)
+            # take mod360 of dome_az to keep 0 <= dome_az < 360
             self.dome_az %= 360
+            # update encoder_count to match the dome_az
             self.encoder_count = self._az_to_ticks(self.dome_az)
             pass
         pass
 
-    def calibrate(self):
+    def calibrate(self, num_cal_rotations=2):
         """Calibrate the encoder (determine degrees per tick)"""
+        # rotate the dome until we hit home, to give reference point
         self._move_cw()
-        self.home_sensor.wait_for_active()
-        time.sleep(0.5)
+        self.home_sensor.wait_for_active(timeout=self.wait_timeout)
+        time.sleep(0.1)
         self._stop_moving()
         self.encoder_count = 0
-        time.sleep(0.5)
-        self._move_cw()
-        # might be dumb but trying to get two rotations worth of encoder ticks
-        self.home_sensor.wait_for_active()
-        time.sleep(0.5)
-        self._stop_moving()
-        time.sleep(0.5)
-        self._move_cw()
-        self.home_sensor.wait_for_active()
-        time.sleep(0.5)
-        self._stop_moving()
-        self.az_per_tick = 360 / (self.encoder_count / 2)
-        pass
 
-    # This will be dome the gRPC python server implementation, which will
-    # create an instance of this dome class
-    #
-    # def start_daemon(self):
-    #     """Maybe start the RCP daemon here?."""
-    #     raise NotImplementedError
-    #
-    # def halt_daemon(self):
-    #     """Maybe start the RCP daemon here?."""
-    #     raise NotImplementedError
+        # now set dome to rotate n times so we can determine the number of
+        # ticks per revolution
+        rotation_count = 0
+        while rotation_count < num_cal_rotations:
+            time.sleep(0.5)
+            self._move_cw()
+            if self.testing:
+                self._simulate_ticks(num_ticks=10)
+            self.home_sensor.wait_for_active(timeout=self.wait_timeout)
+            time.sleep(0.5)
+            self._stop_moving()
+            rotation_count += 1
+
+        self.az_per_tick = 360 / (self.encoder_count / rotation_count)
+        pass
 
 ###############################################################################
 # Private Methods
@@ -232,9 +249,9 @@ class Dome():
     def _set_not_home(self):
         self._at_home = False
 
-    def _increment_count(self, device=None):
+    def _increment_count(self):
         # Unsure what purpose the device variable is supposed to serve here?
-        print(f"{device} activated _increment_count")
+        print(f"Encoder activated _increment_count")
         if self.current_direction == "CW":
             self.encoder_count += 1
         elif self.current_direction == "CCW":
@@ -252,7 +269,7 @@ class Dome():
         return ticks * self.az_per_tick
 
     def _move_cw(self):
-        # print(f"Sending GPIO move_cw({degrees}) command.")
+        self.last_direction = self.current_direction
         self.current_direction = "CW"
         # set the direction relay switch to CW position
         self.direction_relay.on()
@@ -262,7 +279,7 @@ class Dome():
         return cmd_status
 
     def _move_ccw(self):
-        # print(f"Sending GPIO move_ccw({degrees}) command.")
+        self.last_direction = self.current_direction
         self.current_direction = "CCW"
         # set the direction relay switch to CCW position
         self.direction_relay.off()
@@ -275,3 +292,12 @@ class Dome():
         self.rotation_relay.off()
         self.last_direction = self.current_direction
         self.current_direction = None
+
+    def _simulate_ticks(self, num_ticks):
+        tick_count = 0
+        while tick_count < num_ticks:
+            self.encoder_pin.drive_low()
+            time.sleep(self.t_wait)
+            self.encoder_pin.drive_high()
+            time.sleep(self.t_wait)
+            tick_count += 1
