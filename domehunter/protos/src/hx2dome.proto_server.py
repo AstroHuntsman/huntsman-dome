99cc508ededb325e357d4bd829c1543ea050d1a5
Unknown <fergus.longbottom@students.mq.edu.au>
2019-09-06 17:11:28 +1000

    changes to python server

--- domehunter/protos/src/hx2dome.proto_server.py
+++ domehunter/protos/src/hx2dome.proto_server.py
@@ -1,11 +1,13 @@
-from concurrent import futures
-import time
 import logging
+import time
+import sys
+from concurrent import futures
 
 import grpc
 
 import hx2dome_pb2
 import hx2dome_pb2_grpc
+from domehunter import Dome
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
@@ -29,30 +31,29 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 class HX2DomeServer(hx2dome_pb2_grpc.HX2DomeServicer):
 
+    def __init__(self, testing, debug_lights):
+        super(HX2DomeServer, self).__init__()
+        self.dome = Dome(testing=testing, debug_lights=debug_lights)
+
     def dapiGetAzEl(self, request, context):
         print(f'Receiving: GetAzEl request')
-        time.sleep(1)
-
         # our cpp TheSkyX driver uses the dapiGetAzEl rpc to check link
         # need to add code to get az and el from rpi
         # for now just give dummy numbers
         # if something wrong with rpi and no response change return code
         response = hx2dome_pb2.AzEl(return_code=1, az=10.0, el=20.0)
         print(f'Sending: Az={response.az}, El={response.el}')
-        time.sleep(1)
         return response
 
     def dapiGotoAzEl(self, request, context):
         # print something for debugging
         # so I know python server is actually getting a request from client
         print(f'Receiving: GotoAzEl Az={request.az}, El={request.el}')
-        time.sleep(1)
         # increment the return code by 1 so I can see the client/server
         # communication is working
         response = hx2dome_pb2.ReturnCode(return_code=request.return_code)
         print(f'Sending: GotoAzEl complete,'
               f' return code={response.return_code}\n')
-        time.sleep(1)
         return response
 
     def dapiAbort(self, request, context):
@@ -113,9 +114,11 @@ class HX2DomeServer(hx2dome_pb2_grpc.HX2DomeServicer):
         return response
 
 
-def serve():
+def serve(testing, debug_lights):
+    # TODO add shell arguments for testing and debug_lights mode
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    hx2dome_pb2_grpc.add_HX2DomeServicer_to_server(HX2DomeServer(), server)
+    hx2dome_pb2_grpc.add_HX2DomeServicer_to_server(
+        HX2DomeServer(testing, debug_lights), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     try:
@@ -126,5 +129,29 @@ def serve():
 
 
 if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if len(sys.argv) == 3:
+            testing = str_to_bool(sys.argv[1])
+            debug_lights = str_to_bool(sys.argv[2])
+        else:
+            print("""Incorrect number of command line parameters supplied.\n
+                  To run server with defaults try:\n
+                  $ python hx2dome.proto_server.py\n
+                  To toggle testing and debug_lights on try:\n
+                  $ python hx2dome.proto_server.py True True""")
+            sys.exit(1)
+    else:
+        testing = True
+        debug_lights = True
+
     logging.basicConfig()
-    serve()
+    serve(testing, debug_lights)
+
+
+def str_to_bool(string):
+    if string.lower() == 'true':
+        return True
+    elif string.lower() == 'false':
+        return False
+    else:
+        raise ValueError('Command line parameters must be True or False')
